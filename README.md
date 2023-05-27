f8ad4d978d4808775bdaec67e31d6d4ee3831f7d
pyrmon <florian+github@schickel.me>
2023-05-27 02:56:03 +0200

    Added SSM functionality to CDK

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # PowerManagement
+
 Lambda Function to run SSH Requests
+
+I can't create secure strings via CDK, so I will create the Raspi Key via CLI in Github Actions:
+
+```bash
+aws ssm put-parameter \
+--name "/raspi/private" \
+--value "your-secret-value" \
+--type SecureString \
+--overwrite
+```
--- cdk/PowerManagement.Cdk/PowerManagementCdkStack.cs
+++ cdk/PowerManagement.Cdk/PowerManagementCdkStack.cs
@@ -1,15 +1,12 @@
-using System.Collections.Generic;
 using Amazon.CDK;
+using Amazon.CDK.AWS.APIGateway;
 using Amazon.CDK.AWS.EC2;
-using Amazon.CDK.AWS.IAM;
-using Amazon.CDK.AWS.Lambda;
-using Amazon.CDK.AWS.Logs;
 using Constructs;
-using PowerManagement.CDK.Extensions;
+using PowerManagement.CDK.Builders;
 
 namespace PowerManagement.CDK;
 
-public class PowerManagementCdkStack : Stack
+public sealed class PowerManagementCdkStack : Stack
 {
     private const string FunctionName = "PowerManagement-Function";
     private const string LambdaHandler = "PowerManagement.Lambda";
@@ -17,68 +14,9 @@ public class PowerManagementCdkStack : Stack
 
     public PowerManagementCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
     {
-        var lambda = CreateLambda();
+        var lambda = LambdaBuilder.CreateLambda(this, LambdaZipName, FunctionName, LambdaHandler);
         CreateCfnOutput(lambda);
-    }
-
-    private Function CreateLambda()
-    {
-        var code = Code.FromAsset(LambdaZipName);
-        var role = CreateLambdaRole();
-        role.AddManagedPolicy();
-        return new Function(this, FunctionName, new FunctionProps
-        {
-            Code = code,
-            Runtime = Runtime.DOTNET_6,
-            FunctionName = FunctionName,
-            Handler = LambdaHandler,
-            MemorySize = 128,
-            Timeout = Duration.Seconds(180),
-            LogRetention = RetentionDays.ONE_WEEK,
-            Role = role,
-            Architecture = Architecture.X86_64,
-            Tracing = Tracing.ACTIVE
-        });
-    }
-
-    private Role CreateLambdaRole()
-    {
-        return new Role(this, $"{FunctionName}-Role", new RoleProps
-        {
-            AssumedBy = new ServicePrincipal("lambda.amazonaws.com"),
-            RoleName = $"{FunctionName}-ServiceRole",
-            InlinePolicies = new Dictionary<string, PolicyDocument>
-            {
-                {
-                    $"{FunctionName}-Policy", new PolicyDocument(new PolicyDocumentProps
-                    {
-                        Statements = new[]
-                        {
-                            new PolicyStatement(new PolicyStatementProps
-                            {
-                                Actions = CreateAllowedActions(),
-                                Resources = new[] { "*" },
-                                Effect = Effect.ALLOW
-                            })
-                        }
-                    })
-                }
-            }
-        });
-    }
-
-    private static string[] CreateAllowedActions()
-    {
-        return new[]
-        {
-            "lambda:InvokeFunction",
-            "logs:CreateLogGroup",
-            "logs:CreateLogStream",
-            "logs:PutLogEvents",
-            "ssm:GetParametersByPath",
-            "ssm:GetParameter",
-            "kms:DecryptKey"
-        };
+        _ = SsmBuilder.CreateSshConnectionPlanParameter(this, "/ssh/actionPlan");
     }
 
     private void CreateCfnOutput(IClientVpnConnectionHandler lambda)
